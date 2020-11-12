4cd342a5dd395e6738c1f58253800a8681c699c5
Zzazuu <djxeon@live.se>
2020-11-12 09:41:29 +0100

    Revamped the whole resource

--- config.lua
+++ config.lua
@@ -2,7 +2,11 @@
 -- Ayarlar --
 -------------------
 Config = {}
-Config.doktor = 2
+Config.Locale = 'en'
+Config.Doktor = 2 -- Needs to be less then this amount online for it to work
+Config.Price = 2000
+Config.MoneyFormat = '$'
+Config.MythicProgbar = true -- Set to true if you have mythic_progbar, otherwise set to false
 
 doktorPed = {name = "Doktor Hulusi", icon = "CHAR_CALL911", model = "s_m_m_doctor_01", colour = 111}
 
---
+++ locales/en.lua
@@ -0,0 +1,7 @@
+Locales['en'] = {
+    ['getting_treatment'] = "The doctor is giving you medical aid",
+    ['treatment_done'] = "Your treatment is done, you were charged: ",
+    ['to_many_medics'] = "There is to many medics online",
+    ['only_when_dead'] = "This can only be used when dead",
+    ['not_enough_money'] = "You can't afford medical treatment",
+}
